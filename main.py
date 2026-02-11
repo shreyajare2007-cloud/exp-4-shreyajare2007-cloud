6ba92fac8df1f3115827fa824c8822554d36d270
shreyajare2007-cloud <shreyajare2007@gmail.com>
2026-02-11 11:16:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 # AIM: Write a Python program to calculate the simple interest based on user input.
 # Coder:
 # Date:
+principal=float(input("Enter the principal value:"))
+rate=float(input("Enter the rate value:"))
+time=float(input("Enter the time:"))
 
+SI=(principal*rate*time)/100
+print("simple intrest:",SI)
 # Write your code here
